823769fc913381cef4cbf5f83f2fb3aa02632349
nc <luckecor@gmail.com>
2024-05-22 12:20:58 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodoTasksComponent } from './todo-tasks/todo-tasks.component';
+import { ProgressTasksComponent } from './progress-tasks/progress-tasks.component';
+import { TestTasksComponent } from './test-tasks/test-tasks.component';
+import { DoneTasksComponent } from './done-tasks/done-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoTasksComponent,
+    ProgressTasksComponent,
+    TestTasksComponent,
+    DoneTasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
